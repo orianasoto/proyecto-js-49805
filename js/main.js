57acff4a70bd7c7af47be1021d7fb6f0c5712895
orianasoto <orianandreina17@gmail.com>
2023-12-14 21:53:35 -0300

    Segunda preentrega js mas una pequeña estructura basica de html

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 // Lista de tareas
 let tareas = [];
 
+
 // Agregar tarea
-function agregarTarea(tarea) {
-  tareas.push(tarea);
+function agregarTarea(tarea, estado = "pendiente") {
+  const tareaObjeto = { tarea, estado };
+  tareas.push(tareaObjeto);
   console.log(`Tarea "${tarea}" agregada.`);
 }
 
-// Las tareas
+// Mostrar todas las tareas
 function mostrarTareas() {
   console.log("Lista de tareas:");
 
   if (tareas.length === 0) {
     console.log("¡La lista de tareas está vacía!");
   } else {
-    tareas.forEach((tarea, index) => {
-      console.log(`${index + 1}. ${tarea}`);
-    });
+    for (let indice = 0; indice < tareas.length; indice++) {
+      console.log(`${indice + 1}. Tarea: ${tareas[indice].tarea} - Estado: ${tareas[indice].estado}`);
+    }
   }
 }
 
