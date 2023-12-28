cecb7ff491aa11ced0bd8307d2db6d4c7630ef1e
orianasoto <orianandreina17@gmail.com>
2023-12-28 00:50:17 -0300

    realice modificaciones en mi js y html para darle un poco mas de sentido al funcionamiento del DOM

--- js/main.js
+++ js/main.js
@@ -1,37 +1,81 @@
+
 // Lista de tareas
 let tareas = [];
 
+//Ingreso de tareas
+document.addEventListener("DOMContentLoaded", function () {
+  cargarTareas();
+});
 
-// Agregar tarea
-function agregarTarea(tarea, estado = "pendiente") {
-  const tareaObjeto = { tarea, estado };
-  tareas.push(tareaObjeto);
-  console.log(`Tarea "${tarea}" agregada.`);
+function cargarTareas() {
+  const listaTareas = document.getElementById("listaTareas");
+  listaTareas.innerHTML = "";
+
+  const tareas = obtenerTareasDesdeLocalStorage() || [];
+
+
+//Al precionar en la tarea indica si esta pendiente o por completada
+  tareas.forEach((tarea, indice) => {
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = `${indice + 1}. ${tarea.descripcion} - Estado: ${tarea.completada ? 'Completada' : 'Pendiente'}`;
+    nuevaTarea.addEventListener("click", function () {
+      marcarComoCompletada(indice);
+    });
+
+    listaTareas.appendChild(nuevaTarea);
+  });
 }
 
-// Mostrar todas las tareas
-function mostrarTareas() {
-  console.log("Lista de tareas:");
+function agregarTarea() {
+  const nuevaTareaInput = document.getElementById("nuevaTarea");
+  const nuevaTarea = nuevaTareaInput.value.trim();
 
-  if (tareas.length === 0) {
-    console.log("¡La lista de tareas está vacía!");
-  } else {
-    for (let indice = 0; indice < tareas.length; indice++) {
-      console.log(`${indice + 1}. Tarea: ${tareas[indice].tarea} - Estado: ${tareas[indice].estado}`);
-    }
+  if (nuevaTarea !== "") {
+    const tareas = obtenerTareasDesdeLocalStorage() || [];
+    tareas.push({ descripcion: nuevaTarea, completada: false });
+    guardarTareasEnLocalStorage(tareas);
+    cargarTareas();
+    nuevaTareaInput.value = "";
   }
 }
 
-// Interactividad
-while (true) {
-  const Usuario = prompt("Ingrese una tarea (o escriba 'x' para salir):");
+function marcarComoCompletada(indice) {
+  const tareas = obtenerTareasDesdeLocalStorage() || [];
 
-  if (Usuario.toLowerCase() === "x") {
-    console.log("Saliendo del programa. Lista final de tareas:");
-    mostrarTareas();
-    break;
-  } else {
-    agregarTarea(Usuario);
-    mostrarTareas();
+  if (indice >= 0 && indice < tareas.length) {
+    tareas[indice].completada = !tareas[indice].completada;
+    guardarTareasEnLocalStorage(tareas);
+    cargarTareas();
   }
 }
+
+function obtenerTareasDesdeLocalStorage() {
+  const tareasGuardadas = localStorage.getItem("tareas");
+  return JSON.parse(tareasGuardadas);
+}
+
+function guardarTareasEnLocalStorage(tareas) {
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+/*function filtrarTareas(filtro) {
+  const tareas = obtenerTareasDesdeLocalStorage() || [];
+  let tareasFiltradas = [];
+
+  switch (filtro) {
+    case 'pendientes':
+      tareasFiltradas = tareas.filter(tarea => !tarea.completada);
+      break;
+    case 'completadas':
+      tareasFiltradas = tareas.filter(tarea => tarea.completada);
+      break;
+    default:
+      tareasFiltradas = tareas;
+  }
+
+  mostrarTareasEnLista(tareasFiltradas);
+}
+
+function mostrarTareasEnLista(tareas) {
+  const listaTareas = document.getElementById("listaTareas");
+  listaTareas.innerHTML = ""; */
